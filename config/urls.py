3c3b30570a275a08b8d5a6afc5cb755e1db88a23
Roman Titov <romiktitov@gmail.com>
2021-05-29 17:11:27 +0300

    Add paths in urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="vacancies/index.html")),
+    path('vacancies/', TemplateView.as_view(template_name="vacancies/vacancies.html")),
+    path('vacancies/cat/frontend', TemplateView.as_view(template_name="vacancies/vacancies.html")),
+    path('vacancies/<int:pk>', TemplateView.as_view(template_name="vacancies/vacancy.html")),
+    path('companies/<int:pk>', TemplateView.as_view(template_name="vacancies/company.html")),
 ]
